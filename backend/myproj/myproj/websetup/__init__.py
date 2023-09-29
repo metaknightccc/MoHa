c0aa7daa0635949217c83ad65d86d36bdd5357d6
DrPeachy <p1067838263@gmail.com>
2023-09-29 19:51:05 -0400

    feat: Backend basic setup --Charles

---
+++ backend/myproj/myproj/websetup/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""Setup the myproj application"""
+
+import logging
+
+from myproj.config.app_cfg import base_config
+
+__all__ = ['setup_app']
+
+log = logging.getLogger(__name__)
+
+from .schema import setup_schema
+from .bootstrap import bootstrap
+
+
+def setup_app(command, conf, vars):
+    """Place any commands to setup myproj here"""
+    conf = base_config.configure(conf.global_conf, conf.local_conf)
+    base_config.setup(conf)
+
+    setup_schema(command, conf, vars)
+    bootstrap(command, conf, vars)
