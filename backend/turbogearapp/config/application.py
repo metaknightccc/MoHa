f18b7f0373a934fc9d035221f7b65b37b7f03492
VChhh <ql2015@nyu.edu>
2023-09-29 20:18:48 -0400

    update backend

---
+++ backend/turbogearapp/config/application.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""WSGI application initialization for turbogear-app."""
+from turbogearapp.config.app_cfg import base_config
+
+__all__ = ['make_app']
+
+
+def make_app(global_conf, **app_conf):
+    """
+    Set turbogear-app up with the settings found in the PasteDeploy configuration
+    file used.
+
+    :param dict global_conf: The global settings for turbogear-app
+                             (those defined under the ``[DEFAULT]`` section).
+
+    :return: The turbogear-app application with all the relevant middleware
+        loaded.
+
+    This is the PasteDeploy factory for the turbogear-app application.
+
+    ``app_conf`` contains all the application-specific settings (those defined
+    under ``[app:main]``.
+    """
+    app = base_config.make_wsgi_app(global_conf, app_conf, wrap_app=None)
+
+    # Wrap your final TurboGears 2 application with custom middleware here
+
+    return app
---
+++ startTurboGearServer.bat
@@ -0,0 +1,5 @@
+@echo off
+
+cd backend
+echo starting turbo gear server
+gearbox serve --reload --debug
