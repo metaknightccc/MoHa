4ced8e864536c0c4d96f2f5f069c5c2d75e8ae86
YifanSun <sunyifanc@gmail.com>
2023-10-10 01:27:33 -0400

    feat: add a simple login page --Yifan

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import LoginPage from './LoginPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoginPage />
     </div>
   );
 }
