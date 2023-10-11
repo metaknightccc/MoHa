3d38120fce45411b4fe8065eb1bcd05adf8b742f
metaknightccc <sunyifanc@126.com>
2023-10-10 23:30:08 -0400

    fix: test

--- test.py
+++ test.py
@@ -21,6 +21,7 @@ cur.execute("INSERT INTO person (id, name, age, gender) VALUES (2, 'Alice', 25, 
 cur.execute("INSERT INTO person (id, name, age, gender) VALUES (3, 'Xijingping', 26, 'M')")
 cur.execute("INSERT INTO person (id, name, age, gender) VALUES (4, 'Pan C', 26, 'M')")
 cur.execute("INSERT INTO person (id, name, age, gender) VALUES (5, 'JiangZeming', 26, 'M')")
+cur.execute("INSERT INTO person (id, name, age, gender) VALUES (6, 'Dick', 26, 'M')")
 conn.commit()  # Commit the changes
 
 # Select and print data from the 'person' table
