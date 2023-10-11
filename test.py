9e0b8750337d7aa9598da05a8cc3145d453f7bc8
Zongtang Michael Shuai <m.shuai@nyu.edu>
2023-10-10 21:59:40 -0400

    no message

--- test.py
+++ test.py
@@ -12,6 +12,19 @@ cur.execute("""CREATE TABLE IF NOT EXISTS person (
     )
     
 """)
+
 conn.commit()
+
+cur.execute("INSERT INTO person (id, name, age, gender) VALUES (1, 'John', 30, 'M')")
+cur.execute("INSERT INTO person (id, name, age, gender) VALUES (2, 'Alice', 25, 'F')")
+conn.commit()  # Commit the changes
+
+# Select and print data from the 'person' table
+cur.execute("SELECT * FROM person")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+
 cur.close()
 conn.close()
