e83d7eecec90f938c69c07835390e6773b1947bd
Zongtang Michael Shuai <m.shuai@nyu.edu>
2023-10-10 21:38:48 -0400

    feat: postgre testing code

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-#im blue
+#postgre liability test
+import psycopg2
+
+conn = psycopg2.connect(host="localhost", port=5432, dbname="moha", user="postgres", password="doudou8", sslmode="prefer", connect_timeout=10)
+
+cur=conn.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS person (
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    age INT,
+    gender CHAR
+    )
+    
+""")
+conn.commit()
+cur.close()
+conn.close()
