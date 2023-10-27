a39042bcdde27ca14852a3115001b5756e14c150
metaknightccc <sunyifanc@126.com>
2023-10-27 17:32:46 -0400

    feat: add background to homepage --Yifan

--- react-app/src/HomePage.js
+++ react-app/src/HomePage.js
@@ -5,6 +5,7 @@ const HomePage = () => {
   return (
     <div className="HomePage">
       <h1>Home Page</h1>
+      <button class="rounded-pill" type="submit">Get it Started</button>
     </div>
   );
 };
