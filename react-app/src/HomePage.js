0602c2d50e02c2ef990f031041f1811264951ccc
metaknightccc <sunyifanc@126.com>
2023-10-30 11:25:04 -0400

    fix: update homepage --Yifan

--- react-app/src/HomePage.js
+++ react-app/src/HomePage.js
@@ -2,27 +2,110 @@ import React from "react";
 import "./HomePage.css";
 import ClassSlot from "./ClassSlot";
 import background from "./background.svg";
+import Carousel from 'react-bootstrap/Carousel';
 
 const HomePage = () => {
   return (
-    <div className="HomePage">
-      
-      <div className="First">
-        <img src={background} alt="background"/>
+    <div className="wrap">
+      <div className="bgContainer">
+        <div className="First">
+          <img src={background} className="banner" alt="background" />
+          <div className="txt">
+            <h2 className="wlc">Learn Together Grow Together</h2>
+            <h1 className="title">find any desired courses here or become a tutor to help others</h1>
+            <div className="button">Get started now</div>
+          </div>
+        </div>
       </div>
       <div>
-        <ClassSlot/>
+        <Carousel interval={10000} pause={false}>
+          <Carousel.Item>
+            <div className="group">
+            <div className="item">
+              <img
+                className="d-block w-100"
+                src="./assets/nyu.jpg"
+                alt="First slide"
+              />
+              <div className="title">Hello world</div>
+            </div>
+
+            <div className="item">
+              <img
+                className="d-block w-100"
+                src="./assets/nyu.jpg"
+                alt="First slide"
+              />
+              <div className="title">Hello world</div>
+            </div>
+
+            <div className="item">
+              <img
+                className="d-block w-100"
+                src="./assets/nyu.jpg"
+                alt="First slide"
+              />
+              <div className="title">Hello world</div>
+            </div>
+
+            <div className="item">
+              <img
+                className="d-block w-100"
+                src="./assets/nyu.jpg"
+                alt="First slide"
+              />
+              <div className="title">Hello world</div>
+            </div>
+            </div>
+           
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="group">
+            <div className="item">
+              <img
+                className="d-block w-100"
+                src="./assets/nyu.jpg"
+                alt="First slide"
+              />
+              <div className="title">Hello world111</div>
+            </div>
+
+            <div className="item">
+              <img
+                className="d-block w-100"
+                src="./assets/nyu.jpg"
+                alt="First slide"
+              />
+              <div className="title">Hello world111</div>
+            </div>
+
+            <div className="item">
+              <img
+                className="d-block w-100"
+                src="./assets/nyu.jpg"
+                alt="First slide"
+              />
+              <div className="title">Hello world111</div>
+            </div>
+
+            <div className="item">
+              <img
+                className="d-block w-100"
+                src="./assets/nyu.jpg"
+                alt="First slide"
+              />
+              <div className="title">Hello world</div>
+            </div>
+            </div>
+           
+          </Carousel.Item>
+
+          
+         
+        </Carousel>
       </div>
-      <div>
-        <ClassSlot/>
-      </div>
-      
-      
-      {/* <div className="item">
-          <button className="rounded-pill" type="submit">Get it Started</button>
-      </div> */}
-      
     </div>
+
   );
 };
 
