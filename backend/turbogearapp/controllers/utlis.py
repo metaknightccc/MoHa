12e04a27782fd287c68bb6b4ac4e3ebf932f8504
VChhh <ql2015@nyu.edu>
2023-12-09 15:19:21 -0500

    fixed: removed utlis.py

--- backend/turbogearapp/controllers/utlis.py
+++
@@ -1,9 +0,0 @@
-# utility functions
-
-def sqlalchemy_to_json(sqlalchemy_objects):
-    result = []
-    for obj in sqlalchemy_objects:
-        result.append(
-            {column.name: getattr(obj, column.name) for column in obj.__table__.columns}
-        )
-    return result
