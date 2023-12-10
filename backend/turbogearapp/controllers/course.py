a3fa21971446c19abea06c0a85b7f78dc2fb1461
ZongtangShuai <michael.zongtang@gmail.com>
2023-12-10 15:31:52 -0500

    feat: tutor email and name would now be displayed in CourseDes and CourseMain -Michael

--- backend/turbogearapp/controllers/course.py
+++ backend/turbogearapp/controllers/course.py
@@ -101,11 +101,14 @@ class CourseController(TGController):
         #     image_file = open('./turbogearapp/public/assets/default.png', 'rb')
         #     encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
         if course:
+            tutor = DBSession.query(Tutor).filter_by(id=course.tutor_id).first()
             print("====GetCourseInfo2====")
             print(is_enrolled)
             return dict(
                 id=course.id,
                 tutor_id=course.tutor_id,
+                tutor_name=str(tutor.first_name) + ' ' + str(tutor.last_name),
+                tutor_email=tutor.email,
                 name=course.name,
                 subject_name=course.subject_name,
                 type=course.type,
